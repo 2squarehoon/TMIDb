4348ce614d1f6f7716d19b1a8bd7a4a002b2d8f2
SH <shhh1006@daum.net>
2022-05-22 22:04:50 +0900

    Build account, article front

---
+++ final-pjt-front/src/views/ArticleEditView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>Edit Article</h1>
+    <article-form :article="article" action="update"></article-form>
+  </div>
+</template>
+
+<script>
+import ArticleForm from '@/components/ArticleForm.vue'
+
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  name: 'AritcleEditView',
+  components: { ArticleForm },
+  data() {
+    return {
+      articlePk: '',
+    }
+  },
+  computed: {
+    ...mapGetters(['article']),
+  },  
+  methods: {
+    ...mapActions(['fetchArticle'])
+  },
+  created () {
+    console.log('edit')
+    this.articlePk = this.$route.params.articlePk
+    this.fetchArticle({ articlePk: this.articlePk })
+  },
+}
+</script>
+
+<style>
+
+</style>
