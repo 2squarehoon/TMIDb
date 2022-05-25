0b3376422fb910f27bd9c7c0a78c59dcc38566d9
SH <shhh1006@daum.net>
2022-05-26 01:00:37 +0900

    Build MovieDetail css

--- final-pjt-front/src/views/MovieDetailView.vue
+++ final-pjt-front/src/views/MovieDetailView.vue
@@ -1,13 +1,27 @@
 <template>
 <!-- 영화 하나 클릭하면 나오는 상세페이지, 평점 및 댓글을 구현하기 위함. -->
-  <div>
+  <div class="container-fluid">
     <h1>{{ movie.korean_title }}</h1>
-    <p>{{ movie.overview }}</p>
-    <div>
-      Likeit: <button @click="likeMovie({ moviePk: movie.pk })">{{ like_count }}</button>
+    <div class="row">
+      <img :src="movie.poster_url" :alt="movie.korean_title" class="col-8 col-sm-4 offset-2">
+      <div class="col-8 col-sm-4">
+        <h4>장르 : {{ movie.genres }}</h4>
+        <h4>출시연도 : {{ movie.year }}</h4>
+        <h4>예산 : {{ movie.budget }}</h4>
+        <h4>네이버 평점 : {{ movie.naver_rating }} / 10</h4>
+        <h4>왓챠 평점 : {{ movie.watcha_rating }} / 5</h4>
+        <h4>IMDb 평점 : {{ movie.imdb_rating }} / 10</h4>
+        <h4>Metacritic 평점 : {{ movie.mc_rating }} / 100</h4>
+        <h4>Rotten Tomatoes 평점 : {{ movie.rt_rating }} / 100</h4>
+        <hr>
+        <h5>{{ movie.overview }}</h5>
+        <div>
+          좋아요 : <button @click="likeMovie({ moviePk: movie.pk })">{{ like_count }}</button>
+        </div>
+        <hr>
+        <review-list :reviews="movie.reviews"></review-list>
+      </div>
     </div>
-    <hr>
-    <review-list :reviews="movie.reviews"></review-list>
   </div>
 </template>
 
@@ -43,5 +57,7 @@ export default {
 </script>
 
 <style>
-
+.card-img-top {
+  height: 800px;
+}
 </style>
